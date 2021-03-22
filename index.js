aadc5ff4bebf5a6cf21508f201e53784447ce775
Nicolo Traini <n.traini1@yahoo.it>
2021-03-22 13:39:51 +0100

    Add unique validation to phonebook entries, testing online deployment to heroku

--- index.js
+++ index.js
@@ -71,17 +71,22 @@ app.post(
     (request, response) => {
         const body = request.body
         
+        /*
         if (!body.name || !body.number) {
             return response.status(400).json({
                 error: `can't save entry with missing data!`
             })
         }
+        */
 
         const person = new Person({
             name: body.name,
             number: body.number
         })
-        person.save().then(savedPerson => response.json(savedPerson))
+
+        person.save()
+            .then(savedPerson => response.json(savedPerson))
+            .catch(error => next(error))
     }
 )
 
@@ -102,7 +107,9 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
-    } 
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
 
     next(error)
 }
